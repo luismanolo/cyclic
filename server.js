aaa1dab683c0345f2c8d816d3e7dc7c2ae34c344
Bengoa <luismanolo@gmail.com>
2023-08-03 19:07:50 +0200

    Sigue sin funcionar2

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-const express = require('express');
 const path = require('path')
+const express = require('express');
 
 const app = express();
 const PORT = 3000;
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-//
-app.use('/', express.static(path.join(__dirname, 'dist')))
-app.use('/site', express.static(path.join(__dirname, 'dist')))
+(async function() {
+  app.use('/', express.static(path.join(__dirname, 'dist')))
 
-// app.use('/site/', function(req, res, next) {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'))
-//   next();
-// })
+  // app.use('/site', express.static(path.join(__dirname, '/dist')))
+  app.use('/site/assets', express.static(path.join(__dirname, 'dist', 'assets')))
+  app.use('/site', function(req, res){
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+  })
 
-// app.get('/site', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-// });
-// app.use('/site/assets', express.static(path.join(__dirname, 'dist', 'assets')))
+  app.listen(PORT, () =>
+    console.log(`Server listening on port: ${PORT}`),
+  );
 
-
-app.listen(PORT, () =>
-  console.log(`Server listening on port: ${PORT}`),
-);
+})();
